f45463f71f77b8af10d97e5812e4237c34692ff9
vanhoang <hoangray2309@gmail.com>
2024-10-13 23:57:16 +0700

    coithudim

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+// int main() {
+//     printf("Hello, World!\n");
+//     return 0;
+// }
+
+
+#include <stdio.h>
+#define PI 3.14159
+#define AREA(r) (PI*r*r)
+int main(){
+   int radius = 5;
+   float area = AREA(radius);
+   printf("Area: %f", area);
+   return 0;
 }
