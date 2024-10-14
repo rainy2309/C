8085f6b0468e8a9947bd500e9088d02931e157cd
Van Hoang <hoangray2309@gmail.com>
2024-10-14 23:52:39 +0700

    1410

--- main.c
+++ main.c
@@ -17,8 +17,42 @@
 // }
 
 
-#include <stdio.h>  
-int main (){
-   printf("%d la me cua %s\n",12,"thanhcong");
-   printf("%0.3f",12.333121);
+// #include <stdio.h>  
+// int main (){
+//    int age = 12;
+//    double salary=12.3123;
+//    printf("%d la me cua %s\n",age,"thanhcong");
+//    printf("%.1lf",salary);
+//    printf("We are \"TEK4.VN\" and we are free.");
+// }
+
+
+#include <stdio.h>
+  
+int main() { 
+   //Biểu diễn dữ liệu ký tự
+   char ch = 'B'; 
+   printf("%c\n", ch);  
+    
+   //Biểu diễn số nguyên bằng %d hoặc %i
+   int x = 45, y = 90; 
+   printf("%d\n", x); 
+   printf("%i\n", y); 
+   float f = 12.67; 
+   //In số thực 
+   printf("%f\n", f); 
+   printf("%e\n", f); // Biểu diễn số dưới dạng số mũ 10
+    
+   int a = 67; 
+   printf("%o\n", a); // Biểu diễn dưới dạng bát phân
+    
+   printf("%x\n", a); // Biểu diễn dưới dạng thập lục phân (16)
+    
+   char str[] = "Hello World"; 
+   printf("%s\n", str); 
+    
+   printf("%20s\n", str); //Dịch chuyển chuỗi sang phải 20 ký tự
+   printf("%-20s\n", str); // Căn lề bên trái
+   printf("%20.5s\n", str); //shift to the right 20 characters including the string, and print string up to 5 character 
+   printf("%-20.5s\n", str); //left align and print string up to 5 character 
 }
