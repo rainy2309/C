73244abd9670f8689f8f6600cfe88c4fbbce0201
vanhoang <hoangray2309@gmail.com>
2024-10-14 19:39:20 +0700

    them1vaithu

--- main.c
+++ main.c
@@ -6,12 +6,19 @@
 // }
 
 
-#include <stdio.h>
-#define PI 3.14159
-#define AREA(r) (PI*r*r)
-int main(){
-   int radius = 5;
-   float area = AREA(radius);
-   printf("Area: %f", area);
-   return 0;
+// #include <stdio.h>
+// #define PI 3.14159
+// #define AREA(r) (PI*r*r)
+// int main(){
+//    int radius = 5;
+//    float area = AREA(radius);
+//    printf("Area: %f", area);
+//    return 0;
+// }
+
+
+#include <stdio.h>  
+int main (){
+   printf("%d la me cua %s\n",12,"thanhcong");
+   printf("%0.3f",12.333121);
 }
