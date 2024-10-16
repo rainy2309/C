3eeecc18c77a07c1432d9915241a3172ae6045b0
vanhoang <hoangray2309@gmail.com>
2024-10-16 09:02:50 +0700

    bai moi 16-10

--- main.c
+++ main.c
@@ -57,3 +57,17 @@
 //    printf("%-20.5s\n", str); //left align and print string up to 5 character 
 // }
 
+#include <stdio.h>
+#include <math.h>
+int main (){
+    int n;
+    scanf("%d",&n);
+    int tong = 0;
+    while (n!=0)
+    {
+        tong = tong + n%10;
+        n=n/10;
+    }
+    printf("%d",tong);
+    
+}
